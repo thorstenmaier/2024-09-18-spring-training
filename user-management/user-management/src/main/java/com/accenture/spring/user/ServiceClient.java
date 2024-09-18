04e313fc4313ed92812a9b7a09d2bca8478585d2
Thorsten Maier <thorsten.a.maier@accenture.com>
2024-09-18 11:05:03 +0200

    @MyService und Field Injection

--- user-management/user-management/src/main/java/com/accenture/spring/user/ServiceClient.java
+++ user-management/user-management/src/main/java/com/accenture/spring/user/ServiceClient.java
@@ -1,12 +1,29 @@
 package com.accenture.spring.user;
 
 import com.accenture.spring.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ServiceClient {
+public class ServiceClient implements CommandLineRunner {
 
-    public ServiceClient(UserService userService) {
+    @Autowired
+    private UserService userService;
+
+    public ServiceClient() {
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("run");
         userService.getAllUsers().forEach(System.out::println);
+    }
+
+    @EventListener
+    public void listen(ApplicationReadyEvent event) {
+        System.out.println("ApplicationReadyEvent");
     }
 }
