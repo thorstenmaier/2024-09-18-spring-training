bf6e8a783eaccabf3452e073f70482ddca4bef91
Thorsten Maier <thorsten.a.maier@accenture.com>
2024-09-19 15:34:45 +0200

    @Query

--- user-management/user-management/src/main/java/com/accenture/spring/user/repository/UserRepository.java
+++ user-management/user-management/src/main/java/com/accenture/spring/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.accenture.spring.user.repository;
 
 import com.accenture.spring.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Set;
@@ -10,5 +12,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Set<User> findByLastname(String lastname);
     User findByFirstnameAndLastname(String firstname, String lastname);
     List<User> findByLastnameLikeOrderByLastnameDesc(String lastnameLike);
+
+    @Query("from User WHERE firstname like :firstname")
+    Set<User> holMirUser(@Param("firstname") String firstname);
+
 
 }
